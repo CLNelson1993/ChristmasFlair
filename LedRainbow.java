57aceba4c0e51e8c2d7a52a94bcfa84628e583d4
CLNelson1993 <cnelson986@mycr.redwoods.edu>
2023-09-23 15:08:50 -0700

    added LedRainbow.java

---
+++ LedRainbow.java
@@ -0,0 +1,34 @@
+import com.diozero.ws281xj.LedDriverInterface;
+import com.diozero.ws281xj.PixelAnimations;
+import com.diozero.ws281xj.PixelColour;
+import com.diozero.ws281xj.rpiws281x.WS281x;
+
+public class LedRainbow {
+    public static void main(String[] args) {
+        int gpio = 18;
+        int brightness = 255;
+        int numpixels = 25;
+
+        try (LedDriverInterface led = new WS281x(gpio, brightness, numpixels)) {
+            ledRainbow(led);
+            // PixelAnimations.demo(led); // I can't find documentation on 'demo'. Is this part needed for the animation  to work?
+        } catch (Throwable t) {
+            System.out.println("Error: " + t);
+            t.printStackTrace();
+        }
+    }
+    private static void ledRainbow(LedDriverInterface led) {
+        System.out.println("rainbowColours()");
+
+        int[] colours = PixelColour.RAINBOW;
+
+        for (int i = 0; i < 254; i++) {
+            for (int pixel = 0; pixel < led.getNumPixels(); pixel++) {
+                led.setPixelColour(pixel, colours[(i + pixel) % colours.length]);
+            }
+
+            led.render();
+            PixelAnimations.delay(50);
+        }
+    }
+}
