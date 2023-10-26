7a21009d0e8a97b2eb0598986105a15365ae95ff
CLNelson1993 <cnelson986@mycr.redwoods.edu>
2023-10-26 13:39:02 -0700

    Todd Howard approved Command Design Pattern (it just works)

--- src/Receiver.java
+++ src/Receiver.java
@@ -2,6 +2,7 @@ public class Receiver {
     public void concreteCommandMethod() {
         System.out.println("concreteCommandMethod() executed");
     }
+
     public void cmdRainbow() {
         System.out.println("cmdRainbow() executed");
     }
