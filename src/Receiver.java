b4849f88f0e73380d2674890706cf034fd464751
CLNelson1993 <cnelson986@mycr.redwoods.edu>
2023-10-26 21:12:44 -0700

    README.md updated.

--- src/Receiver.java
+++ src/Receiver.java
@@ -4,10 +4,9 @@ import com.diozero.ws281xj.PixelColour;
 import com.diozero.ws281xj.rpiws281x.WS281x;
 
 public class Receiver {
-
-    //initialize WS281x Strip. 18 = gpionum, 255 = brightness, 100 = pixelnum.
-    private LedDriverInterface ledDriver = new WS281x(18, 255, 100); //should this be public, private, or nothing at all?
-
+    //Initialize LED strip by assigning gpionum, brightness & numPixels
+    //QUESTION: should this be public, private, or nothing at all?
+    private LedDriverInterface ledDriver = new WS281x(18, 255, 100);
 
     //Rainbow method
     public void cmdRainbow() {
