768333e4a2cfba405b631cd897cc487dac87a45a
CLNelson1993 <cnelson986@mycr.redwoods.edu>
2023-10-28 10:59:52 -0700

    added scanner to Client for testing purposes.

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         //create receiver (you only need one of these for all commands)
@@ -9,8 +11,37 @@ public class Client {
         //create invoker to pass the command
         Invoker rainbowInvoker = new Invoker(cmdRainbow);
 
+        //execute cmdRainbow()
+        //rainbowInvoker.execute();
 
-        //execute cmdRainbow().
-        rainbowInvoker.execute();
+
+
+        // Scanner for testing purposes
+        // Create Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            //prompt
+            System.out.println("Enter a number to choose an animation.");
+            System.out.println("1. Rainbow");
+            System.out.println("0. Close program");
+
+            //scan for integer
+            int userInput = scanner.nextInt();
+
+            //choices
+            switch (userInput) {
+                case 1:
+                    //execute cmdRainbow().
+                    rainbowInvoker.execute();
+                    break;
+                case 0:
+                    System.out.println("Closing program...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("That's not an option.");
+            }
+        }
     }
 }
