a79b0c4a088f0a5ab5e58cc8b85a7e66808a4061
CLNelson1993 <cnelson986@mycr.redwoods.edu>
2023-11-01 11:22:37 -0700

    README.md update

---
+++ src/main/java/com/st6/ChristmasFlair/ChristmasFlairApplication.java
@@ -0,0 +1,13 @@
+package com.st6.ChristmasFlair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasFlairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasFlairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/st6/ChristmasFlair/ChristmasFlairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st6.ChristmasFlair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasFlairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
